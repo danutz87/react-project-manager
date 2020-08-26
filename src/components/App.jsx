f66f378f513616597aab0eda8a0ea6300542cc9a
daniel ghimpu <danielghimpu@yahoo.com>
2020-08-26 14:04:12 +0100

    Add component done

---
+++ src/components/App.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import uuid from 'uuid';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: []
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      projects: [
+        {
+          id: uuid.v4(),
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          id: uuid.v4(),
+          title: 'Social App',
+          category: 'Mobile Dev'
+        },
+        {
+          id: uuid.v4(),
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Dev'
+        }
+      ]
+    });
+  }
+
+  handleAddProject (project) {
+    const { projects } = this.state.projects;
+    projects.push(project);
+    this.setState({
+      projects: projects
+    });
+  }
+
+  handleDeleteProject(id) {
+    const projects = this.state.projects;
+    const index = projects.findIndex(x => x.id);
+    projects.splice(index, 1);
+    this.setState({
+      projects: projects
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddProject addProject={this.handleAddProject.bind(this)} />
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+export default App;
